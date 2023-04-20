f64490f02848f90a3377718b011b6accae854df0
Raquel Martínez <raquel.mrti@gmail.com>
2023-04-21 01:35:33 +0200

    add functionality when user clicks on an option

--- README.md
+++ README.md
@@ -14,3 +14,4 @@ Here I will track my progress through it and possibly add some notes.
 - Made the `getPokemonOptions.js` helper which contains a few functions needed to fetch the 4 Pokémons and their names.
 - Finished all the logic for `getPokemonOptions`. Basically we start an array with 650 positions, map through it to fill all the positions with their index value, sort it randomly, pass the first four numbers to a function that makes an API call to fetch the Pokémon whose IDs match the numbers, and return their names and IDs in an object.
 - Made `PokemonOptions` render the list of four Pokémon names.
+- Added functionality that makes the Pokémon image show up once the user clicks on one of the options. In order for this to happen, each of the options in `PokemonOptions` emits a `selection` event to its parent component `PokemonPage` when clicked on. Then, `PokemonPage` executes a function that changes the value of `showPokemon` from `false` to `true`.
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,7 +1,10 @@
 <template>
-  <h1>Who is this Pokémon?</h1>
-  <PokemonPicture :pokemonId='10' :showPokemon="true" />
-  <PokemonOptions :pokemons='pokemonArr' />
+  <h1 v-if='!pokemon'>Please wait...</h1>
+  <div v-else>
+    <h1>Who is this Pokémon?</h1>
+    <PokemonPicture :pokemonId='pokemon.id' :showPokemon='showPokemon' />
+    <PokemonOptions :pokemons='pokemonArr' @selection='checkAnswer' />
+  </div>
 </template>
 
 <script>
@@ -19,12 +22,19 @@ export default {
   data() {
     return {
       pokemonArr: [],
+      pokemon: null,
+      showPokemon: false
     }
   },
   methods: {
     async mixPokemonArray() {
       this.pokemonArr = await getPokemonOptions()
-      console.log(this.pokemonArr)
+      const rndInt = Math.floor(Math.random() * 4)
+      // Select one Pokemon from the pokemonArr randomly, this will be the correct answer
+      this.pokemon = this.pokemonArr[rndInt]
+    },
+    checkAnswer(pokeId) {
+      this.showPokemon = true
     }
   },
   mounted() {
